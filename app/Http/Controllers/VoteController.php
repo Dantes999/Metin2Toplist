b89610c9c51d1312ec43d84ba2bf6695c5d96701
Volvox <admin@server.de>
2024-09-18 20:31:33 +0200

    refactor

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -133,8 +133,6 @@ class VoteController extends Controller
         $apiToken = $request->apiToken;
         $accountId = $request->accountId;
 
-        return ['apiToken' => $apiToken, 'accountId' => $accountId];
-
         if (isset($apiToken) && isset($accountId)) {
             $validator = Validator::make(['apiToken' => $apiToken, 'accountId' => $accountId], [
                 'apiToken' => 'required|alpha_num',
